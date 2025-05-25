5e62b1dfa7144594757a90511a7b25ca843b57e2
Roy Abbink <accounts@royabbink.com>
2025-05-25 13:12:31 +0200

    Starting fresh

--- script.js
+++
@@ -1,5 +0,0 @@
-// Fetch the current year and update the copyright text
-document.addEventListener('DOMContentLoaded', function() {
-    const currentYear = new Date().getFullYear();
-    document.getElementById('currentYear').textContent = currentYear;
-});
